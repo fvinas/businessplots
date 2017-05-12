3803f4a1de9fc692b1322933088b1862ebfb7df8
Fabien Vinas <fabien@ehda.co>
2017-05-12 21:19:41 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(
+	name="businessplots",
+	version="0.1",
+	description="A matplotlib extension to help draw business quality charts",
+	url="https://github.com/fvinas/businessplots",
+	author="Fabien Vinas",
+	author_email="fabien.vinas@gmail.com",
+	license="",
+	packages=["businessplots"],
+	install_requires=[
+		"matplotlib"
+	],
+	zip_safe=False
+)	
